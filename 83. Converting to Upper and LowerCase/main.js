5b180798d882492f5c613e3588ec9b384a219e91
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-17 19:10:17 +0500

    push on github

---
+++ 83. Converting to Upper and LowerCase/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function convertCase(str) {
+    let upperStr = str.toUpperCase();
+    let lowerStr = str.toLowerCase();
+    console.log("UpperCase:", upperStr, "Lowercase:", lowerStr);
+}
+convertCase("Hello World");
---
+++ 83. Converting to Upper and LowerCase/main.ts
@@ -0,0 +1,6 @@
+function convertCase(str: string) {
+    let upperStr = str.toUpperCase();
+    let lowerStr = str.toLowerCase();
+    console.log("UpperCase:", upperStr,"Lowercase:", lowerStr); 
+}
+convertCase("Hello World");
