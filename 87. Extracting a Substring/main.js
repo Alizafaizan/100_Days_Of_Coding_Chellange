8eaf22694a4c595837361c7c49a5ba89d682612d
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-19 16:01:12 +0500

    Q: 87 done

---
+++ 87. Extracting a Substring/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function ExtractFirstTenCharc(str) {
+    return str.substring(0, 10);
+}
+console.log(ExtractFirstTenCharc("Hello, javaScript world!"));
---
+++ 87. Extracting a Substring/main.ts
@@ -0,0 +1,4 @@
+function ExtractFirstTenCharc(str: string): string {
+    return str.substring(0, 10);
+}
+console.log(ExtractFirstTenCharc("Hello, javaScript world!"));
