81691332c5b3d8c866224db65a54cf98c9ba7ac3
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-19 15:53:46 +0500

    Q:  86 done

---
+++ 86. Checking for Text Presence/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function checkingForText(str) {
+    return str.includes("TypeScript");
+}
+console.log(checkingForText("I love coding in TypeScript!"));
---
+++ 86. Checking for Text Presence/main.ts
@@ -0,0 +1,4 @@
+function checkingForText(str: string): boolean {
+    return str.includes("TypeScript");
+}
+console.log(checkingForText("I love coding in TypeScript!"));
