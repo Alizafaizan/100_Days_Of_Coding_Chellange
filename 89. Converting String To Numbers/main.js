4527aa5faf82d342f9a3b681b2c24fb5951b6b79
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-19 16:16:48 +0500

    Q: 89 done

---
+++ 89. Converting String To Numbers/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function convertStringToNumbers(str) {
+    return parseFloat(str);
+}
+console.log(convertStringToNumbers("123.45"));
+console.log(convertStringToNumbers("98"));
---
+++ 89. Converting String To Numbers/main.ts
@@ -0,0 +1,6 @@
+function convertStringToNumbers(str: string): number {
+    return parseFloat(str);
+}
+console.log(convertStringToNumbers("123.45"));
+console.log(convertStringToNumbers("98"));
+
