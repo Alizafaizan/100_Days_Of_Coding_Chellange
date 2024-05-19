5349be0f054782e966d184d9e5130c5ae8954955
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-19 16:08:10 +0500

    Day 30 started and Q: 88 is completed

---
+++ 88. Rounding to the Nearest Integer/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(4.7));
---
+++ 88. Rounding to the Nearest Integer/main.ts
@@ -0,0 +1,4 @@
+function roundToNearestInteger(num: number): number{
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(4.7));
