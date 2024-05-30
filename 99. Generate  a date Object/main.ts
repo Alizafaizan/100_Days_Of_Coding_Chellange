5f8bb16b628ac3c55db7f486b692b34545d33ee0
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-31 01:00:14 +0500

    Q: 99 done

---
+++ 99. Generate  a date Object/main.ts
@@ -0,0 +1,12 @@
+
+ function getNextBirthday(month: number, day: number): Date {
+    const today = new Date();
+    let year = today.getFullYear();
+    const Birthday = new Date(year, month - 1, day);
+    if(Birthday < today) {
+        Birthday.setFullYear(year + 1);
+    }    
+    return Birthday;
+}
+const nextBirthday = getNextBirthday(2, 10);
+console.log("Next Birthday on:", nextBirthday.toLocaleDateString());
