ffdd0b7098d0ab529987a97a18974df7566b23ee
Aliza Faizan <alizafaizan86@gmail.com>
2024-05-31 01:25:54 +0500

    Q: 102 done

---
+++ 102. Math.abs( ) Function/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Difference between -5 and 5
+const difference = Math.abs(-5 - 5);
+console.log(difference);
---
+++ 102. Math.abs( ) Function/main.ts
@@ -0,0 +1,4 @@
+// Difference between -5 and 5
+
+const difference: number = Math.abs(-5 - 5);
+console.log(difference);
