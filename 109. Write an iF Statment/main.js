45f9f901bc00c0662ed2c19c5e04c6ecaf34521c
Aliza Faizan <alizafaizan86@gmail.com>
2024-06-01 19:46:36 +0500

    push

--- 109. Write an iF Statment/main.js
+++ 109. Write an iF Statment/main.js
@@ -1,5 +1,10 @@
 "use strict";
 const currentTime = new Date();
+if (currentTime.getHours() < 12) {
+    console.log("Good Morning");
+}
+console.log("------------------------------");
+const currentTime1 = new Date();
 if (currentTime.getHours() > 12) {
     console.log("Good Night");
 }
--- 109. Write an iF Statment/main.ts
+++ 109. Write an iF Statment/main.ts
@@ -2,3 +2,9 @@ const currentTime = new Date();
 if(currentTime.getHours() < 12) {
     console.log("Good Morning");   
 }
+console.log("------------------------------");
+
+const currentTime1 = new Date();
+if(currentTime.getHours() > 12) {
+    console.log("Good Night");   
+}
