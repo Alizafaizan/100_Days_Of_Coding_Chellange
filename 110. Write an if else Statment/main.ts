d548585a0b2ba211e24da89e71655496252f8140
Aliza Faizan <alizafaizan86@gmail.com>
2024-06-01 19:55:59 +0500

    Q: 110 done

---
+++ 110. Write an if else Statment/main.ts
@@ -0,0 +1,15 @@
+function assignGrade(score: number): string {
+    if (score >= 90) {
+        return "A";
+    }else if (score >= 80) {
+        return "B";
+    }else if (score >= 70) {
+        return "C"; 
+    }else if (score >= 60) {
+        return "D"; 
+    }else {
+        return "F";  
+    }
+}
+console.log(assignGrade(85));
+console.log(assignGrade(55));
