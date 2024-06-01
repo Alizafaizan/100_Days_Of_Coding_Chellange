f3a522651b9337ef400d29b1cd9d9f31afd89646
Aliza Faizan <alizafaizan86@gmail.com>
2024-06-01 19:24:50 +0500

    Q: 107 done

---
+++ 107. Logical operators checks if a number divisible by 2 and 3/main.ts
@@ -0,0 +1,15 @@
+function isDivisibleByTwoAndThree(number: number): boolean {
+    return number % 2 === 0 && number % 3 === 0;
+}
+console.log(isDivisibleByTwoAndThree(12));
+console.log(isDivisibleByTwoAndThree(14));
+console.log("-----------------------------------");
+
+function isDivisibleByFourAndFive(number: number): boolean {
+    return number % 4 === 0 && number % 5 === 0;
+}
+console.log(isDivisibleByFourAndFive(20));
+console.log(isDivisibleByFourAndFive(24));
+
+
+
