4b2e74e59461671e84ee246a34dc00c8ca4b0bd0
Aliza Faizan <alizafaizan86@gmail.com>
2024-06-01 18:31:00 +0500

    Q: 104 done

---
+++ 104. Hexadecimal color code/main.ts
@@ -0,0 +1,5 @@
+function getRandomHexColor(): string {
+    const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+    return color;
+}
+console.log(getRandomHexColor());
