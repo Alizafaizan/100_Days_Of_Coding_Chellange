e3de05e0caabe9e1b896d0847b870303365a28d5
Aliza Faizan <alizafaizan86@gmail.com>
2024-06-02 16:58:05 +0500

    Q:  112 done

---
+++ 112. Create a Map to Store Countries and their Capital/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let Countries = new Map();
+Countries.set("USA", "Washington, D.C");
+Countries.set("France", "Paris");
+Countries.set("Japan", "Tokyo");
+console.log(Countries);
---
+++ 112. Create a Map to Store Countries and their Capital/main.ts
@@ -0,0 +1,7 @@
+let Countries = new Map<string, string>();
+Countries.set("USA", "Washington, D.C");
+Countries.set("France", "Paris");
+Countries.set("Japan", "Tokyo");
+
+console.log(Countries);
+
